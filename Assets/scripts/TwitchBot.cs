e849bdcb0d621e24f862933a72435fdc6e197a80
Vitalii Vodkevych <tirect@gmail.com>
2017-01-17 16:40:18 +0200

    ужалил бота, теперь он в юнити.

---
+++ Assets/scripts/TwitchBot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TwitchIRC))]
+public class TwitchBot : MonoBehaviour {
+
+	private TwitchIRC IRC;
+
+	void OnChatMsgRecieved(string msg){
+		Debug.Log(msg);
+	}
+
+	// Use this for initialization
+	void Start () {
+		IRC = this.GetComponent<TwitchIRC>();
+		IRC.messageRecievedEvent.AddListener(OnChatMsgRecieved);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
